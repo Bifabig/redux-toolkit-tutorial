3cf075bdec26b4f47c07fb7c5f4ff183c30015cf
Bifabig <biftubig@gmail.com>
2023-06-13 17:06:48 +0300

    done with redux toolkit cart tutorial

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import CartContainer from './components/CartContainer';
+import Navbar from './components/Navbar';
+import { useDispatch, useSelector } from 'react-redux';
+import { calculateTotals, getCartItems } from './features/cart/cartSlice';
+import { useEffect } from 'react';
+import Modal from './components/Modal';
+
+function App() {
+  const { cartItems, isLoading } = useSelector((store) => store.cart);
+  const { isOpen } = useSelector((store) => store.modal);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [dispatch, cartItems]);
+
+  useEffect(() => {
+    dispatch(getCartItems());
+  }, [dispatch]);
+
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <h1>isLoading</h1>
+      </div>
+    );
+  }
+  return (
+    <main>
+      {isOpen && <Modal />}
+      <Navbar />
+      <CartContainer />
+    </main>
+  );
+}
+export default App;
